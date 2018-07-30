87928e298daa8b43dc79cfe8e1b0d367054169eb
Theriley106 <christopherlambert106@gmail.com>
2018-07-30 08:13:24 -0700

    Added credential files

---
+++ credentials.py
@@ -0,0 +1,23 @@
+import os
+
+BASE_PATH = "credentials/{0}"
+SQL_USERNAME_PATH = BASE_PATH.format("sql_username.txt")
+SQL_PASSWORD_PATH = BASE_PATH.format("sql_password.txt")
+
+def get_sql_username():
+	if os.path.isfile(SQL_USERNAME_PATH):
+		return open(SQL_USERNAME_PATH).read().split("\n")[0]
+	else:
+		credential_val = str(raw_input("Input SQL Username: ")).strip()
+		with open(SQL_USERNAME_PATH, 'w') as the_file:
+    		the_file.write(credential_val)
+    	return credential_val
+
+def get_sql_password():
+	if os.path.isfile(SQL_PASSWORD_PATH):
+		return open(SQL_PASSWORD_PATH).read().split("\n")[0]
+	else:
+		credential_val = str(raw_input("Input SQL Password: ")).strip()
+		with open(SQL_PASSWORD_PATH, 'w') as the_file:
+    		the_file.write(credential_val)
+    	return credential_val
---
+++ credentials/README.md
@@ -0,0 +1,1 @@
+# This is the location of all credentials required for using this application
