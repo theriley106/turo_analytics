d96f549dca63c3469b2b977d41d55597b1381383
Theriley106 <christopherlambert106@gmail.com>
2018-07-31 08:12:28 -0700

    Got long lat

--- grab.py
+++ grab.py
@@ -5,7 +5,7 @@ import credentials
 
 conn = psycopg2.connect(host="ec2-54-243-129-189.compute-1.amazonaws.com", database="dbfncufnkimb1n", user=credentials.get_sql_username(), password=credentials.get_sql_password())
 cursor = conn.cursor()
-cursor.execute("SELECT vehicle_model FROM turodb WHERE vehicle_year = 2013")
+cursor.execute("SELECT (location_longitude, location_latitude) FROM turodb WHERE vehicle_make = 'Tesla'")
 for val in cursor.fetchall():
   print val[0]
 conn.commit()
