91fdd1241b2836c8be105b75b8bc3f01e45eef0f
Theriley106 <christopherlambert106@gmail.com>
2018-10-15 16:06:31 -0400

    Added better single vehicle searching

--- turo.py
+++ turo.py
@@ -136,6 +136,17 @@ class search(object):
 		cursor.close()
 		return allResults
 
+	def searchSingleID(self, vehicle_id):
+		allResults = []
+		conn = psycopg2.connect(host="ec2-54-243-129-189.compute-1.amazonaws.com", database="dbfncufnkimb1n", user=credentials.get_sql_username(), password=credentials.get_sql_password())
+		cursor = conn.cursor()
+		cursor.execute("SELECT vehicle_name, rate_daily, location_address, vehicle_image_thumbnails_620x372, vehicle_year FROM turodb WHERE vehicle_id = {}".format(int(vehicle_id)))
+		for val in cursor.fetchall():
+		  	allResults.append(val)
+		conn.commit()
+		cursor.close()
+		return allResults
+
 	def searchUserID(self, user_id):
 		allResults = []
 		conn = psycopg2.connect(host="ec2-54-243-129-189.compute-1.amazonaws.com", database="dbfncufnkimb1n", user=credentials.get_sql_username(), password=credentials.get_sql_password())
@@ -176,7 +187,7 @@ class search(object):
 		allResults = []
 		conn = psycopg2.connect(host="ec2-54-243-129-189.compute-1.amazonaws.com", database="dbfncufnkimb1n", user=credentials.get_sql_username(), password=credentials.get_sql_password())
 		cursor = conn.cursor()
-		cursor.execute("SELECT location_longitude, location_latitude FROM turodb WHERE vehicle_make = 'Tesla'")
+		cursor.execute("SELECT location_longitude, location_latitude FROM turodb WHERE vehicle_make = '{}'".format(model))
 		for val in cursor.fetchall():
 			a, b = re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", str(val))
 		  	allResults.append([b, a])
