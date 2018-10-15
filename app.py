3c2079d0f2193c8e708ec1d42a7c03f0dbaae157
Theriley106 <christopherlambert106@gmail.com>
2018-10-15 16:10:47 -0400

    Modified again to actually return all values from db

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, Markup, js
 import json
 import turo
 import os
+ALL_KEYS = ['rating', 'owner_image_thumbnails_225x225', 'location_precision_accuracy', 'vehicle_name', 'location_timeZone', 'rate_monthly', 'distanceLabel', 'vehicle_marketCountry', 'rate_averageDailyPrice', 'vehicle_listingCreatedTime', 'vehicle_marketCurrency_decimalPlaces', 'owner_image_id', 'vehicle_marketCurrency_currencyCode', 'images_0_thumbnails_170x125', 'images_0_id', 'rate_averageDailyPriceWithCurrency_currencyCode', 'images_0_thumbnails_574x343', 'owner_lastName', 'rate_averageDailyPriceWithCurrency_amount', 'vehicle_image_verified', 'location_city', 'vehicle_id', 'location_country', 'vehicle_image_thumbnails_50x30', 'location_precision_level', 'owner_image_thumbnails_84x84', 'location_longitude', 'owner_image_placeholder', 'vehicle_make', 'images_0_verified', 'images_0_originalImageUrl', 'images_0_thumbnails_50x30', 'owner_image_thumbnails_32x32', 'vehicle_url', 'rentableFromSearchedAirport', 'vehicle_marketCurrency_defaultFractionDigits', 'responseRate', 'vehicle_image_placeholder', 'owner_image_originalImageUrl', 'rate_daily', 'deliveryLabel', 'vehicle_year', 'vehicle_image_originalImageUrl', 'reviewCount', 'owner_name', 'renterTripsTaken', 'location_addressLines_0', 'vehicle_image_resizableUrlTemplate', 'images_0_thumbnails_100x60', 'location_locationSource', 'rate_weekly', 'owner_id', 'location_address', 'images_0_resizableUrlTemplate', 'vehicle_registration', 'vehicle_image_thumbnails_170x125', 'responseTime', 'businessClass', 'vehicle_marketCurrency_symbol', 'distanceWithUnit_unit', 'vehicle_type', 'owner_image_verified', 'vehicle_image_thumbnails_620x372', 'newListing', 'distance', 'images_0_thumbnails_620x372', 'owner_image_resizableUrlTemplate', 'distanceWithUnit_scalar', 'location_state', 'vehicle_trim', 'images_0_thumbnails_170x102', 'vehicle_automaticTransmission', 'vehicle_image_thumbnails_100x60', 'distanceWithUnit_unlimited', 'freeDeliveryPromotion', 'vehicle_image_thumbnails_574x343', 'vehicle_image_id', 'owner_image_thumbnails_300x300', 'location_latitude', 'owner_firstName', 'vehicle_image_thumbnails_170x102', 'instantBookDisplayed', 'vehicle_model', 'images_0_placeholder']
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -63,12 +64,11 @@ def getAllMakes():
 @app.route('/vehicle/<id_val>', methods=['GET'])
 def getSingleVehicle(id_val):
 	info = {}
-	f = ['vehicle_name', 'rate_daily', 'location_address', 'vehicle_image_thumbnails_620x372', 'vehicle_year']
 	e = turo.search()
 	vals = e.searchSingleID(id_val)[0]
 	print vals
 	for i, val in enumerate(vals):
-		info[f[i]] = val
+		info[ALL_KEYS[i]] = val
 	print info
 	return render_template("resultPage.html")
 
